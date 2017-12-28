53ef1dff7e9986cda1a0bfe4dcda221c69f0b8d5
abhinav272 <abhinav.ih@gmail.com>
2017-12-28 15:20:07 +0530

    utils created

---
+++ app/src/main/java/com/abhinav/asthetic/utils/FailureResponse.kt
@@ -0,0 +1,6 @@
+package com.abhinav.asthetic.utils
+
+/**
+ * Created by abhinav.sharma on 27/12/17.
+ */
+data class FailureResponse(var errorMessage: String, var errorCode: Int)
---
+++ app/src/main/java/com/abhinav/asthetic/utils/ResourceUtil.kt
@@ -0,0 +1,18 @@
+package com.abhinav.asthetic.utils
+
+import android.graphics.drawable.Drawable
+import com.abhinav.asthetic.Aesthetic
+
+
+/**
+ * Created by abhinav.sharma on 28/12/17.
+ */
+object ResourcesUtil {
+    private val context = Aesthetic.instance
+    private val theme = Aesthetic.instance.theme
+
+    fun getDrawableById(resId: Int): Drawable = context.resources.getDrawable(resId, theme)
+
+    fun getString(resId: Int): String = context.resources.getString(resId)
+
+}

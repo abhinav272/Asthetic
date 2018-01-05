93c51e40960a2cf94c0cf1e8ac3df7b418d55dd9
abhinav272 <abhinav.ih@gmail.com>
2018-01-05 20:56:23 +0530

    extends BaseActivity and fragment added

--- app/src/main/java/com/abhinav/asthetic/OnBoardActivity.kt
+++ app/src/main/java/com/abhinav/asthetic/OnBoardActivity.kt
@@ -2,11 +2,15 @@ package com.abhinav.asthetic
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.abhinav.asthetic.base.BaseActivity
+import com.abhinav.asthetic.ui.ShowCollectionFragment
+import kotlinx.android.synthetic.main.activity_on_board.*
 
-class OnBoardActivity : AppCompatActivity() {
+class OnBoardActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_board)
+        addFragment(ShowCollectionFragment(), R.id.frame_container, "test")
     }
 }

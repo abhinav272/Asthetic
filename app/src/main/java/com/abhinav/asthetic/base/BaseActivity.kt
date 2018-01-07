dc95c14ae5d21ebabc34b492954d2a5061671045
abhinav272 <abhinav.ih@gmail.com>
2018-01-07 13:30:12 +0530

    base view created for BaseActivity

--- app/src/main/java/com/abhinav/asthetic/base/BaseActivity.kt
+++ app/src/main/java/com/abhinav/asthetic/base/BaseActivity.kt
@@ -1,22 +1,49 @@
 package com.abhinav.asthetic.base
 
+import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.widget.RelativeLayout
+import com.abhinav.asthetic.R
 import com.abhinav.asthetic.utils.FailureResponse
+import kotlinx.android.synthetic.main.activity_base.*
 
 /**
  * Created by abhinav.sharma on 28/12/17.
  */
-open class BaseActivity : AppCompatActivity(), BaseView {
+abstract class BaseActivity : AppCompatActivity(), BaseView {
 
-    fun addFragment(fragment: Fragment, layoutResId: Int, tag : String) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+        setLayout()
+    }
+
+    private fun setLayout() {
+        if (getResourceId() != -1) {
+            removeLayout()
+            val layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.MATCH_PARENT)
+            val view = layoutInflater.inflate(getResourceId(), null)
+            base_container.addView(view, layoutParams)
+        }
+    }
+
+    abstract fun getResourceId(): Int
+
+    private fun removeLayout() {
+        if (base_container.childCount >= 1)
+            base_container.removeAllViews()
+    }
+
+    fun addFragment(fragment: Fragment, layoutResId: Int, tag: String) {
         supportFragmentManager.beginTransaction()
                 .add(layoutResId, fragment, tag)
                 .commit()
     }
 
-    fun addFragmentWithBackStack(fragment: Fragment, layoutResId: Int, tag: String){
+    fun addFragmentWithBackStack(fragment: Fragment, layoutResId: Int, tag: String) {
         supportFragmentManager.beginTransaction()
                 .add(layoutResId, fragment, tag)
                 .addToBackStack(tag)
@@ -24,7 +51,7 @@ open class BaseActivity : AppCompatActivity(), BaseView {
     }
 
     override fun showSnackbar(msg: String) {
-        Snackbar.make(window.decorView.rootView, msg, Snackbar.LENGTH_LONG).show()
+        Snackbar.make(base_container, msg, Snackbar.LENGTH_LONG).show()
     }
 
     override fun showToastLong(msg: String) {

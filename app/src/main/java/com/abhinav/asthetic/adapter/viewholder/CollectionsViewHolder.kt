bfce88139ab33fbc97675c7a9c5f0eccb5530e11
abhinav272 <abhinav.ih@gmail.com>
2018-01-15 22:53:02 +0530

    binding updated

--- app/src/main/java/com/abhinav/asthetic/adapter/viewholder/CollectionsViewHolder.kt
+++ app/src/main/java/com/abhinav/asthetic/adapter/viewholder/CollectionsViewHolder.kt
@@ -1,10 +1,14 @@
 package com.abhinav.asthetic.adapter.viewholder
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import com.abhinav.asthetic.network.pojo.Collection
 import com.abhinav.asthetic.utils.load
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.layout_collection_single_item.view.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by abhinav.sharma on 08/01/18.
@@ -12,15 +16,37 @@ import kotlinx.android.synthetic.main.layout_collection_single_item.view.*
 class CollectionsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(collection: Collection, listener: (Collection) -> Unit) = with(itemView) {
-        iv_collection.layoutParams.height = collection.height
+        //        iv_collection.layoutParams.height = collection.height
 
-        iv_collection.setOnClickListener {
-            listener.invoke(collection)
-        }
         if (collection.projectCovers != null && !collection.projectCovers!!.isEmpty()) {
-            iv_collection.load(collection.projectCovers!![0].url!!) { requestCreator ->
-                requestCreator.fit().centerCrop()
-            }
+//            iv_collection_1.load(collection.projectCovers!![0].url!!) { requestCreator ->
+//                requestCreator.fit().centerCrop()
+//            }
+//
+//            if (collection.projectCovers!!.size > 1)
+//                iv_collection_2.load(collection.projectCovers!![1].url!!) { requestCreator ->
+//                    requestCreator.fit().centerCrop()
+//                }
+//
+//            if (collection.projectCovers!!.size > 2)
+//                iv_collection_3.load(collection.projectCovers!![2].url!!) { requestCreator ->
+//                    requestCreator.fit().centerCrop()
+//                }
+//
+//            if (collection.projectCovers!!.size > 3)
+//                iv_collection_4.load(collection.projectCovers!![3].url!!) { requestCreator ->
+//                    requestCreator.fit().centerCrop()
+//                }
+//            Observable.interval(2, TimeUnit.SECONDS)
+//                    .repeat()
+//                    .observeOn(AndroidSchedulers.mainThread())
+//                    .subscribe { t: Long? ->
+//                        if (collection.projectCovers!!.size > t!!) {
+//                            iv_collection.load(collection.projectCovers!![t.toInt()].url!!) { requestCreator ->
+//                                requestCreator.fit().centerCrop()
+//                            }
+//                        }
+//                    }
         }
     }
 }

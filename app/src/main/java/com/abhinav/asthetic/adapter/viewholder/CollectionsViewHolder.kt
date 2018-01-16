242d9411f89aa1699ef70ae7bdb4d34eb9894447
abhinav272 <abhinav.ih@gmail.com>
2018-01-16 14:07:15 +0530

    viewholder data binded for owner image and name

--- app/src/main/java/com/abhinav/asthetic/adapter/viewholder/CollectionsViewHolder.kt
+++ app/src/main/java/com/abhinav/asthetic/adapter/viewholder/CollectionsViewHolder.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import android.view.View
 import com.abhinav.asthetic.network.pojo.Collection
 import com.abhinav.asthetic.utils.load
+import com.abhinav.asthetic.utils.loadCircular
+import com.abhinav.asthetic.utils.loadRoundedCorner
+import com.abhinav.asthetic.utils.picasso_utils.RoundedCornersTransformation
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.layout_collection_single_item.view.*
@@ -19,9 +22,9 @@ class CollectionsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) 
         //        iv_collection.layoutParams.height = collection.height
 
         if (collection.projectCovers != null && !collection.projectCovers!!.isEmpty()) {
-//            iv_collection_1.load(collection.projectCovers!![0].url!!) { requestCreator ->
-//                requestCreator.fit().centerCrop()
-//            }
+            iv_collection_1.load(collection.projectCovers!![0].url!!) { requestCreator ->
+                requestCreator.fit().centerCrop()
+            }
 //
 //            if (collection.projectCovers!!.size > 1)
 //                iv_collection_2.load(collection.projectCovers!![1].url!!) { requestCreator ->
@@ -47,6 +50,37 @@ class CollectionsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) 
 //                            }
 //                        }
 //                    }
+        }
+
+        tv_owner_name.text = collection.owners?.get(0)?.displayName
+        if (collection.owners!!.isNotEmpty()) {
+            when {
+                !collection.owners!![0].images!!.fullImage.isNullOrBlank() ->
+                    iv_owner.loadCircular(collection.owners?.get(0)?.images?.fullImage!!) { requestCreator ->
+                        requestCreator.fit().centerCrop()
+                    }
+
+                !collection.owners!![0].images!!.xtraLarge.isNullOrBlank() ->
+                    iv_owner.loadRoundedCorner(collection.owners?.get(0)?.images?.xtraLarge!!) { requestCreator ->
+                        requestCreator.fit().centerCrop()
+                    }
+                !collection.owners!![0].images!!.large.isNullOrBlank() ->
+                    iv_owner.loadRoundedCorner(collection.owners?.get(0)?.images?.large!!) { requestCreator ->
+                        requestCreator.fit().centerCrop()
+                    }
+                !collection.owners!![0].images!!.medium.isNullOrBlank() ->
+                    iv_owner.loadRoundedCorner(collection.owners?.get(0)?.images?.medium!!) { requestCreator ->
+                        requestCreator.fit().centerCrop()
+                    }
+                !collection.owners!![0].images!!.small.isNullOrBlank() ->
+                    iv_owner.loadRoundedCorner(collection.owners?.get(0)?.images?.small!!) { requestCreator ->
+                        requestCreator.fit().centerCrop()
+                    }
+                !collection.owners!![0].images!!.xtraSmall.isNullOrBlank() ->
+                    iv_owner.loadRoundedCorner(collection.owners?.get(0)?.images?.xtraSmall!!) { requestCreator ->
+                        requestCreator.fit().centerCrop()
+                    }
+            }
         }
     }
 }

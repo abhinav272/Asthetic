03bdadc7f8b44d275f789fd19cc2abb62759238d
abhinav272 <abhinav.ih@gmail.com>
2018-01-16 14:07:44 +0530

    extension methods for Picasso

--- app/src/main/java/com/abhinav/asthetic/utils/Extension.kt
+++ app/src/main/java/com/abhinav/asthetic/utils/Extension.kt
@@ -1,10 +1,13 @@
 package com.abhinav.asthetic.utils
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import com.abhinav.asthetic.utils.picasso_utils.CropCircleTransformation
+import com.abhinav.asthetic.utils.picasso_utils.RoundedCornersTransformation
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.RequestCreator
 import java.util.*
@@ -21,6 +24,15 @@ val Context.picasso: Picasso get() = Picasso.with(this)
 
 fun ImageView.load(path: String, request: (RequestCreator) -> RequestCreator) {
     request(context.picasso.load(path)).into(this)
+}
+
+fun ImageView.loadRoundedCorner(path: String, request: (RequestCreator) -> RequestCreator) {
+    request(context.picasso.load(path).transform(RoundedCornersTransformation(20, 5)))
+            .into(this)
+}
+
+fun ImageView.loadCircular(path: String, request: (RequestCreator) -> RequestCreator) {
+    request(context.picasso.load(path).transform(CropCircleTransformation())).into(this)
 }
 
 fun ClosedRange<Int>.random() =

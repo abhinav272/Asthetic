29c46453d43ae565cc4a7be440b36f3149728f2f
abhinav272 <abhinav.ih@gmail.com>
2018-04-21 13:56:09 +0530

    view created for details

---
+++ app/src/main/java/com/abhinav/asthetic/ui/collections/ShowCollectionItemDetailsView.kt
@@ -0,0 +1,16 @@
+package com.abhinav.asthetic.ui.collections
+
+import com.abhinav.asthetic.base.BaseView
+import com.abhinav.asthetic.network.pojo.Collection
+import com.abhinav.asthetic.network.pojo.LatestProject
+
+/**
+ * Created by appinventiv on 19/4/18.
+ */
+interface ShowCollectionItemDetailsView: BaseView {
+
+    fun showCollectionDetails(collection: Collection)
+    fun populateProjectList(projectList: List<LatestProject>)
+    fun getCollectionBaseFromHost():Collection
+
+}

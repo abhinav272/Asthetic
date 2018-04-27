149e4215a4969bee508fa86bb5199f5ae1d09870
abhinav272 <abhinav.ih@gmail.com>
2018-04-27 18:31:30 +0530

    host interface created to fetch base collection object

--- app/src/main/java/com/abhinav/asthetic/ui/collections/CollectionItemDetailHost.kt
+++ app/src/main/java/com/abhinav/asthetic/ui/collections/CollectionItemDetailHost.kt
@@ -1,6 +1,7 @@
 package com.abhinav.asthetic.ui.collections
 
 import com.abhinav.asthetic.network.pojo.Collection
+import com.abhinav.asthetic.network.pojo.LatestProject
 
 /**
  * Created by appinventiv on 21/4/18.

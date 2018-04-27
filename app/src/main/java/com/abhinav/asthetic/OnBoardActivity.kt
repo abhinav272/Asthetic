ba803f63e62e61d7624778de72ed4bb8bfc5fcf9
abhinav272 <abhinav.ih@gmail.com>
2018-04-27 18:35:23 +0530

    methods added for showing Project listing fragment

--- app/src/main/java/com/abhinav/asthetic/OnBoardActivity.kt
+++ app/src/main/java/com/abhinav/asthetic/OnBoardActivity.kt
@@ -1,16 +1,23 @@
 package com.abhinav.asthetic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.abhinav.asthetic.base.BaseActivity
 import com.abhinav.asthetic.ui.ShowCollectionFragment
-import kotlinx.android.synthetic.main.activity_on_board.*
+import com.abhinav.asthetic.ui.collections.ShowCollectionItemDetailsFragment
 
 class OnBoardActivity : BaseActivity() {
     override fun getResourceId(): Int = R.layout.activity_on_board
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        addFragment(ShowCollectionFragment(), R.id.frame_container, "test")
+        showCollectionFragment()
+    }
+
+    fun showCollectionFragment() {
+        addFragment(ShowCollectionFragment(), R.id.frame_container, "ShowCollectionFragment")
+    }
+
+    fun showCollectionDetailFragment() {
+        addFragment(ShowCollectionItemDetailsFragment(), R.id.frame_container, "ShowCollectionItemDetailsFragment")
     }
 }

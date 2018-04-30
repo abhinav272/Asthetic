64bfcb2f12841df706014c4962112de9d71acd8c
abhinav272 <abhinav.ih@gmail.com>
2018-04-30 13:23:07 +0530

    interfaces implementation added, CollectionDetailFragment wiredup

--- app/src/main/java/com/abhinav/asthetic/OnBoardActivity.kt
+++ app/src/main/java/com/abhinav/asthetic/OnBoardActivity.kt
@@ -2,10 +2,15 @@ package com.abhinav.asthetic
 
 import android.os.Bundle
 import com.abhinav.asthetic.base.BaseActivity
+import com.abhinav.asthetic.network.pojo.Collection
 import com.abhinav.asthetic.ui.ShowCollectionFragment
+import com.abhinav.asthetic.ui.collections.CollectionDetailHost
+import com.abhinav.asthetic.ui.collections.CollectionItemDetailHost
 import com.abhinav.asthetic.ui.collections.ShowCollectionItemDetailsFragment
 
-class OnBoardActivity : BaseActivity() {
+class OnBoardActivity : BaseActivity(), CollectionDetailHost, CollectionItemDetailHost {
+
+    private lateinit var collection: Collection
     override fun getResourceId(): Int = R.layout.activity_on_board
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,13 @@ class OnBoardActivity : BaseActivity() {
     }
 
     fun showCollectionDetailFragment() {
-        addFragment(ShowCollectionItemDetailsFragment(), R.id.frame_container, "ShowCollectionItemDetailsFragment")
+        addFragmentWithBackStack(ShowCollectionItemDetailsFragment(), R.id.frame_container, "ShowCollectionItemDetailsFragment")
     }
+
+    override fun onCollectionSelected(collection: Collection) {
+        this.collection = collection
+        showCollectionDetailFragment()
+    }
+
+    override fun getCollection(): Collection = collection
 }

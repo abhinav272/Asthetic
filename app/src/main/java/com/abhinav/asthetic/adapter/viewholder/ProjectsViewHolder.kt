d60c3a01131c4c04653c3aec9ea29e252a708fe0
abhinav272 <abhinav.ih@gmail.com>
2018-05-06 18:55:34 +0530

    fallback approach refactor and image loading added

--- app/src/main/java/com/abhinav/asthetic/adapter/viewholder/ProjectsViewHolder.kt
+++ app/src/main/java/com/abhinav/asthetic/adapter/viewholder/ProjectsViewHolder.kt
@@ -3,6 +3,7 @@ package com.abhinav.asthetic.adapter.viewholder
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.abhinav.asthetic.network.pojo.LatestProject
+import com.abhinav.asthetic.utils.load
 import kotlinx.android.synthetic.main.layout_project_single_item.view.*
 
 class ProjectsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -16,9 +17,15 @@ class ProjectsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             1 -> project.owners!![0].displayName
             else -> "Multiple Users"
         }
-
-        tv_fields.text = project.fields?.get(0) ?: "Fallback Fields"
+        if (project.fields?.isNotEmpty()!!)
+            tv_fields.text = project.fields?.get(0)
+        else tv_fields.text = "Fallback"
         tv_likes.text = project.stats?.appreciations.toString()
         tv_views.text = project.stats?.views.toString()
+        project.covers?.original?.let {
+            iv_project.load(it) { requestCreator ->
+                requestCreator.fit().centerCrop()
+            }
+        }
     }
 }

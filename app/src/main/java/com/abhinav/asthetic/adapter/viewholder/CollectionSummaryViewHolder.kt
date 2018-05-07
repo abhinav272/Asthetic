c15fdc1eea82fe66125e71539b7e8624c1b54f0c
abhinav272 <abhinav.ih@gmail.com>
2018-05-07 18:37:34 +0530

    separate view holder created for collection summary

---
+++ app/src/main/java/com/abhinav/asthetic/adapter/viewholder/CollectionSummaryViewHolder.kt
@@ -0,0 +1,16 @@
+package com.abhinav.asthetic.adapter.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.abhinav.asthetic.network.pojo.Collection
+
+/**
+ * Created by appinventiv on 7/5/18.
+ */
+open class CollectionSummaryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    var rootLayout: View = itemView.rootView
+
+    fun bind(collection: Collection) {
+
+    }
+}

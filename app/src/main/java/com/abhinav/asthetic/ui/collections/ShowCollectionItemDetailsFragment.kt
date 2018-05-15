7947a73a3a8ba9422c00768120dfd58f264541cf
abhinav272 <abhinav.ih@gmail.com>
2018-05-15 18:02:03 +0530

    ui refactor

--- app/src/main/java/com/abhinav/asthetic/ui/collections/ShowCollectionItemDetailsFragment.kt
+++ app/src/main/java/com/abhinav/asthetic/ui/collections/ShowCollectionItemDetailsFragment.kt
@@ -3,7 +3,6 @@ package com.abhinav.asthetic.ui.collections
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup

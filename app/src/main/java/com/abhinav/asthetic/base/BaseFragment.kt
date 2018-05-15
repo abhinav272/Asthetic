93e7398dfd563ed9171499615855e5b45b02f186
abhinav272 <abhinav.ih@gmail.com>
2018-05-15 11:29:02 +0530

    comment removed after bug fix

--- app/src/main/java/com/abhinav/asthetic/base/BaseFragment.kt
+++ app/src/main/java/com/abhinav/asthetic/base/BaseFragment.kt
@@ -1,6 +1,5 @@
 package com.abhinav.asthetic.base
 
-import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.widget.Toast
 import com.abhinav.asthetic.R
@@ -10,20 +9,7 @@ import com.abhinav.asthetic.utils.ResourceUtil
 /**
  * Created by abhinav.sharma on 28/12/17.
  */
-open class BaseFragment: Fragment(), BaseView{
-
-    /**
-     * activity getting null
-     * Caused by: kotlin.TypeCastException: null cannot be cast to non-null type com.abhinav.asthetic.base.BaseActivity
-    at com.abhinav.asthetic.base.BaseFragment.showSnackbar(BaseFragment.kt:16)
-    at com.abhinav.asthetic.base.BaseFragment.showError(BaseFragment.kt:36)
-    at com.abhinav.asthetic.base.BasePresenter.commonError(BasePresenter.kt:34)
-    at com.abhinav.asthetic.base.BaseModel.onCommonError(BaseModel.kt:40)
-    at com.abhinav.asthetic.base.BaseModel.onError(BaseModel.kt:24)
-     *
-     * Closing fragment before snackbar getting generated
-     *
-     * */
+open class BaseFragment : Fragment(), BaseView {
 
     override fun showSnackbar(msg: String) {
         (activity as BaseActivity).showSnackbar(msg)

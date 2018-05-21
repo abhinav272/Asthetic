2f43a442e0c418edea8a4ae40f6f5a25eb69f88f
abhinav272 <abhinav.ih@gmail.com>
2018-05-21 17:43:29 +0530

    callback method to update UI

--- app/src/main/java/com/abhinav/asthetic/ui/collections/ShowCollectionView.kt
+++ app/src/main/java/com/abhinav/asthetic/ui/collections/ShowCollectionView.kt
@@ -2,12 +2,14 @@ package com.abhinav.asthetic.ui.collections
 
 import com.abhinav.asthetic.base.BaseView
 import com.abhinav.asthetic.network.pojo.Collection
+import com.abhinav.asthetic.network.pojo.Owner
 
 /**
  * Created by abhinav.sharma on 30/12/17.
  */
-interface ShowCollectionView: BaseView {
+interface ShowCollectionView : BaseView {
     fun populateCollections(collections: List<Collection>)
     fun showFooterLoader()
     fun hideFooterLoader()
+    fun showCreativesList(creativesToFollowList: List<Owner>?)
 }

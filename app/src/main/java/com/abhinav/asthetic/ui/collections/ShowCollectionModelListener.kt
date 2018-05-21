63a2febe5db01a92cc384487fd635ed7a894d2d1
abhinav272 <abhinav.ih@gmail.com>
2018-05-21 17:42:41 +0530

    callback on success api

--- app/src/main/java/com/abhinav/asthetic/ui/collections/ShowCollectionModelListener.kt
+++ app/src/main/java/com/abhinav/asthetic/ui/collections/ShowCollectionModelListener.kt
@@ -2,10 +2,12 @@ package com.abhinav.asthetic.ui.collections
 
 import com.abhinav.asthetic.base.BaseModelListener
 import com.abhinav.asthetic.network.pojo.Collection
+import com.abhinav.asthetic.network.pojo.Owner
 
 /**
  * Created by abhinav.sharma on 05/01/18.
  */
 interface ShowCollectionModelListener: BaseModelListener {
     fun onCollectionsLoaded(collections: List<Collection>?)
+    fun onCreativesToFollowLoaded(creativesToFollowResponse: List<Owner>?)
 }

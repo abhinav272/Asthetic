ca674595b711c7eb9fa55ec33eec7b1889fde5c7
abhinav272 <abhinav.ih@gmail.com>
2018-06-28 10:22:24 +0530

    product detail fragment wired

--- app/src/main/java/com/abhinav/asthetic/OnBoardActivity.kt
+++ app/src/main/java/com/abhinav/asthetic/OnBoardActivity.kt
@@ -3,10 +3,12 @@ package com.abhinav.asthetic
 import android.os.Bundle
 import com.abhinav.asthetic.base.BaseActivity
 import com.abhinav.asthetic.network.pojo.Collection
-import com.abhinav.asthetic.ui.collections.ShowCollectionFragment
+import com.abhinav.asthetic.network.pojo.LatestProject
 import com.abhinav.asthetic.ui.collections.CollectionDetailHost
 import com.abhinav.asthetic.ui.collections.CollectionItemDetailHost
+import com.abhinav.asthetic.ui.collections.ShowCollectionFragment
 import com.abhinav.asthetic.ui.collections.ShowCollectionItemDetailsFragment
+import com.abhinav.asthetic.ui.projects.ProjectDetailFragment
 
 class OnBoardActivity : BaseActivity(), CollectionDetailHost, CollectionItemDetailHost {
 
@@ -19,11 +21,11 @@ class OnBoardActivity : BaseActivity(), CollectionDetailHost, CollectionItemDeta
     }
 
     fun showCollectionFragment() {
-        addFragment(ShowCollectionFragment(), R.id.frame_container, "ShowCollectionFragment")
+        addFragment(ShowCollectionFragment(), R.id.frame_container, ShowCollectionFragment::class.java.simpleName)
     }
 
     fun showCollectionDetailFragment() {
-        addFragmentWithBackStack(ShowCollectionItemDetailsFragment(), R.id.frame_container, "ShowCollectionItemDetailsFragment")
+        addFragmentWithBackStack(ShowCollectionItemDetailsFragment(), R.id.frame_container, ShowCollectionItemDetailsFragment::class.java.simpleName)
     }
 
     override fun onCollectionSelected(collection: Collection) {
@@ -32,4 +34,8 @@ class OnBoardActivity : BaseActivity(), CollectionDetailHost, CollectionItemDeta
     }
 
     override fun getCollection(): Collection = collection
+
+    override fun showProjectDetails(project: LatestProject) {
+        addFragmentWithBackStack(ProjectDetailFragment.getInstance(project.id), R.id.frame_container, ProjectDetailFragment::class.java.simpleName)
+    }
 }
